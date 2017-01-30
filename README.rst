3a9a0d5a31c442527b338310b76515036a72be07
mq <mquade@uni-potsdam.de>
2017-01-29 18:53:37 -0800

    init

---
+++ README.rst
@@ -0,0 +1,25 @@
+sparsereg
+=========
+
+.. image:: https://travis-ci.org/Ohjeah/sparsereg.svg?branch=master
+    :target: https://travis-ci.org/Ohjeah/symfeat
+.. image:: https://badge.fury.io/py/sparsereg.svg
+    :target: https://badge.fury.io/py/sparsereg
+
+**sparsereg** is a collection of modern sparse (regularized) linear regression algorithms.
+
+
+Features
+--------
+-
+
+Installation
+------------
+
+``pip install sparsereg``
+
+
+Usage
+-----
+
+.. code-block:: python
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from os.path import dirname
+
+from setuptools import setup
+
+here = os.path.abspath(dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as f:
+    long_description = '\n' + f.read()
+
+base_dir = os.path.dirname(__file__)
+
+about = {}
+with open(os.path.join(base_dir, "sparsereg", "__version__.py")) as f:
+    exec(f.read(), about)
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+required = [
+    "numpy",
+    "scikit-learn",
+]
+
+setup(
+    name='sparsereg',
+    version=about['__version__'],
+    description='Modern sparse linear regression',
+    long_description=long_description,
+    author='Markus Quade',
+    author_email='info@markusqua.de',
+    url='https://github.com/ohjeah/sparsereg',
+    packages=['sparsereg'],
+    install_requires=required,
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

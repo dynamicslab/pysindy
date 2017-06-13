cc38eb3b1e53cdce1f8882f4f4dd406231fc14ae
mq <mquade@uni-potsdam.de>
2017-06-13 12:40:11 -0700

    improve net speed

--- example/net.py
+++ example/net.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_boston
-from sklearn.linear_model import Lasso, Ridge
+from sklearn.linear_model import Lasso
 import matplotlib.pyplot as plt
 import numpy as np
 import symfeat as sf
@@ -9,19 +9,25 @@ from sparsereg.net import net
 data = load_boston()
 x, y = data.data, data.target
 
-ests = [Lasso, Ridge, SINDy]
-attrs = ["alpha", "alpha", "knob"]
-names = ["Lasso", "Ridge", "SINDy"]
+
+exponents = [1]
+operators = {}
+
+sym = sf.SymbolicFeatures(exponents=exponents, operators=operators)
+features = sym.fit_transform(x)
+
+ests = [Lasso, SINDy]
+attrs = ["alpha", "knob"]
+names = ["Lasso", "SINDy"]
 
 
-for i, (est, attr, name) in enumerate(zip(ests, attrs, names)):
+for est, attr, name in zip(ests, attrs, names):
 
-    models = net(Lasso, x, y, "alpha", filter=True)
-
+    models = net(est, features, y, attr, filter=True, max_coarsity=5, r_max=1e5)
     m = sorted(models)
-    scores = np.array([models[k].score(x, y) for k in m])
+    scores = np.array([models[k].score(features, y) for k in m])
 
-    plt.plot(m, scores + i*0.05 , 'o--', label=name)
+    plt.plot(m, scores, 'o--', label=name)
 
 plt.legend()
 plt.xlabel("# coefficient")
--- sparsereg/net.py
+++ sparsereg/net.py
@@ -15,51 +15,56 @@ def net(estimator, x, y, attr="alpha", max_coarsity=2, filter=True, r_max=1e3, *
     models = defaultdict(list)
 
     def fit_in_memory(r):
-        est = estimator(**{**kw, **{attr: r}}).fit(x, y)
-        c = complexity(est)
-        memory[c].append(r)
-        models[c].append(est)
-        return est
+        if not any(r in memory[k] for k in memory):
+            est = estimator(**{**kw, **{attr: r}}).fit(x, y)
+            c = complexity(est)
+            memory[c].append(r)
+            models[c].append(est)
+            return c
+
     
     fit_in_memory(0)
     while True:
         try:
-            fit_in_memory(r_max)
-            break
+            c = fit_in_memory(r_max)
+            if c == 0:
+                r_max *= 0.8
+            else:
+                break
         except FitFailedWarning:
             r_max *= 0.8
+
 
     # greedy forward
     def greed_forward(c_lower, c_upper, coarsity):
         upper = min(memory[c_lower])
         lower = max(memory[c_upper])
-        delta = 1./(coarsity*(upper - lower)*(c_upper - c_lower))
-        for r in np.linspace(lower + delta, upper, 1./delta)[::-1]:
+        for r in np.linspace(lower, upper, 2**coarsity)[::-1]:
             fit_in_memory(r)
     
-
     # greedy search for transitions
 
     coarsity = 1
     all_expected = list(range(min(memory), max(memory) + 1))
 
     while True:
-        it = zip(sorted(memory.keys()), all_expected)
-        for discovered, expected in it:
-            if discovered != expected:
-                n_keys = len(memory)
-                greed_forward(max([k for k in memory if k < expected]), discovered, coarsity)
-                if n_keys < len(memory):
-                    coarsity = 1
-                else:
-                    coarsity += 1
-                    if coarsity >= max_coarsity:
-                        all_expected.pop(all_expected.index(expected))
-                        coarsity = 1
-                break
-        else:
+        it = ((d, e) for d, e in zip(sorted(memory.keys()), all_expected) if d != e)
+        try:
+            discovered, expected = next(it)
+        except StopIteration:
             break
-            
+        n_keys = len(memory)
+        greed_forward(max([k for k in memory if k < expected]), discovered, coarsity)
+        if n_keys < len(memory):
+            coarsity = 0
+        else:
+            coarsity += 1
+            if coarsity >= max_coarsity:
+                if discovered == max(all_expected):
+                    break
+                else:
+                    all_expected.pop(all_expected.index(expected))
+                    coarsity = 0
     if filter:
         return {k: min(v, key=lambda x: getattr(x, attr)) for k, v in models.items()}
     else:

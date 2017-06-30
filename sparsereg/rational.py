3681e1a01b933c4e9eba3007258b4eac955f687b
mq <mquade@uni-potsdam.de>
2017-06-29 17:06:06 -0700

    write rationalfunctionswrapper as a mixin to play better with sklearn Pipelines

--- sparsereg/rational.py
+++ sparsereg/rational.py
@@ -1,23 +1,15 @@
 import numpy as np
 
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
-from sklearn.base import BaseEstimator, RegressorMixin
 
 
-class RationalFunctionWrapper(BaseEstimator, RegressorMixin):
-    def __init__(self, base_estimator):
-        if not base_estimator.fit_intercept:
-            raise ValueError
-        self.estimator = base_estimator
-
+class RationalFunctionMixin():
     def _transform(self, x, y):
         return np.hstack((x, y.reshape(-1, 1) * x))
     
     def fit(self, x, y):
         x, y = check_X_y(x, y, multi_output=False)
-        self.estimator.fit(self._transform(x, y), y)
-        self.intercept_ = self.estimator.intercept_
-        self.coef_ = self.estimator.coef_
+        super().fit(self._transform(x, y), y)
         l = len(self.coef_)//2
         self.coef_nominator_ = self.coef_[:l]
         self.coef_denominator_ = -self.coef_[l:]
--- test/test_rational.py
+++ test/test_rational.py
@@ -1,19 +1,23 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
-from sparsereg.rational import RationalFunctionWrapper
+from sparsereg.rational import RationalFunctionMixin
+
+class RLM(RationalFunctionMixin, LinearRegression):
+    pass
 
 
-def test_rational_function_wrapper():
+def test_rational():
 
     def f(x):
         return 1/(1+x)
     
     x = np.linspace(2, 3, 10).reshape(-1, 1)
-    y = f(x)
+    y = f(x).ravel()
 
-    model = RationalFunctionWrapper(LinearRegression())
+    model = RLM()
     model.fit(x, y)
+
     np.testing.assert_almost_equal(model.intercept_, 1.0)
     np.testing.assert_allclose(model.coef_, np.array([0.0, -1]), atol=1e-10)
 

e68f8b42cc0a915d521f3efaafdee7c8b1335c31
Markus Quade <mquade@uni-potsdam.de>
2017-08-22 10:10:21 +0200

    test for cs

---
+++ test/util/test_pipeline.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+import pytest
+
+from sparsereg.util.pipeline import ColumnSelector
+
+cases = ((np.random.random(10), 0),
+         (np.random.random((10, 2)), 1),
+         (np.random.random((10, 2), slice(None)))
+        )
+
+@pytest.mark.parametrize("case", cases)
+def test_ColumnSelector(case):
+    x, index = case
+    assert len(ColumnSelector(index).fit_transform(x).shape) == 2

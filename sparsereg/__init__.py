d12870c016ae05a7bc9bdcb771201c6e71c797c7
mq <mquade@uni-potsdam.de>
2017-11-05 21:04:30 +0100

    versioneer

--- sparsereg/__init__.py
+++ sparsereg/__init__.py
@@ -1,3 +1,7 @@
 from sparsereg.model import *
 from sparsereg.preprocessing import *
 from sparsereg.util import *
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

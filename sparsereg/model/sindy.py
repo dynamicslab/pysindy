c34d4dd2e3135657ce5a5c7e31897a6b43a14d15
TarenGorman <tarengorman@gmail.com>
2017-11-18 15:20:42 -0800

    sindy model features (#13)
    * sindy model features
    
    * vectorized for loop
    
    * fix if conditional

--- sparsereg/model/sindy.py
+++ sparsereg/model/sindy.py
@@ -5,33 +5,50 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures, FunctionTransformer
 
+from sparsereg.preprocessing.symfeat import SymbolicFeatures
 from sparsereg.model.base import STRidge, equation
 
 
 def _derivative(x, dt=1.0):
-    dx = np.zeros_like(x)
-    dx[1:-1, :] = (x[2:, :] - x[:-2, :]) / (2.*dt)
+    if type(dt) in (list, np.ndarray) and len(dt) >= 3:
+        dx = np.zeros_like(x)
+        dx[1:-1, :] = (x[2:, :] - x[:-2, :]) / (dt[2:] - dt[:-2])
 
-    dx[0, :] = (x[1, :] - x[0, :]) / dt
-    dx[-1, :] = (x[-1, :]-x[-2, :]) / dt
+        dx[0, :] = (x[1, :] - x[0, :]) / (dt[1] - dt[0])
+        dx[-1, :] = (x[-1, :] - x[-2, :]) / (dt[-1] - dt[-2])
+    else:
+        dx = np.zeros_like(x)
+        dx[1:-1, :] = (x[2:, :] - x[:-2, :]) / (2.*dt)
+
+        dx[0, :] = (x[1, :] - x[0, :]) / dt
+        dx[-1, :] = (x[-1, :] - x[-2, :]) / dt
 
     return dx
 
 
 class SINDy(BaseEstimator):
-    def __init__(self, dt=1.0, alpha=1.0, threshold=0.1, degree=3, n_jobs=1, derivative=None, feature_names=None, kw={}):
+    def __init__(self, alpha=1.0, threshold=0.1, degree=3, operators=None, dt=1.0, n_jobs=1, derivative=None, feature_names=None, kw={}):
         self.alpha = alpha
         self.threshold = threshold
         self.degree = degree
+        self.operators = operators
         self.n_jobs = n_jobs
         self.derivative = derivative or FunctionTransformer(func=_derivative, kw_args={"dt": dt})
         self.feature_names = feature_names
         self.kw = kw
 
     def fit(self, x, y=None):
-        xdot = self.derivative.transform(x)
+        if y is not None:
+            xdot = y
+        else:
+            xdot = self.derivative.transform(x)
 
-        steps = [("features", PolynomialFeatures(degree=self.degree, include_bias=False)),
+        if self.operators is not None:
+            feature_transformer = SymbolicFeatures(exponents=np.linspace(1, self.degree, self.degree), operators=self.operators)
+        else:
+            feature_transformer = PolynomialFeatures(degree=self.degree, include_bias=False)
+
+        steps = [("features", feature_transformer),
                  ("model", STRidge(alpha=self.alpha, threshold=self.threshold, **self.kw))]
         self.model = MultiOutputRegressor(Pipeline(steps), n_jobs=self.n_jobs)
         self.model.fit(x, xdot)

ba93e228af5be332b3cb144a5c319b2b3f851583
Markus Quade <markus.quade@gmail.com>
2018-02-22 22:33:19 +0100

    fix test

--- test/model/test_stridge.py
+++ test/model/test_stridge.py
@@ -6,7 +6,7 @@ from sparsereg.model import STRidge
 test_settings = (
     dict(threshold=0.5, copy_x=True, normalize=True, fit_intercept=True),
     dict(threshold=0.1, copy_x=True, normalize=False, fit_intercept=True),
-    dict(threshold=0.1, alpha=0, copy_x=True, normalize=True, fit_intercept=True),
+    dict(threshold=0.1, alpha=0, copy_X=True, normalize=True, fit_intercept=True),
 )
 
 

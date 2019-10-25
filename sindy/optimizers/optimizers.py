51f044262e99087ac965a98f2467d5148a59dad4
briandesilva <bdesilva@uw.edu>
2019-10-24 18:10:37 -0700

    Refactored optimizers. Added LASSO optimizer

--- sindy/optimizers/optimizers.py
+++ sindy/optimizers/optimizers.py
@@ -1,37 +1,88 @@
 import warnings
-from itertools import repeat
+import abc
 
 import numpy as np
 from sklearn.base import RegressorMixin
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.exceptions import FitFailedWarning
-from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import ridge_regression
+from sklearn.linear_model import Lasso
 from sklearn.linear_model.base import _rescale_data
 from sklearn.linear_model.base import LinearModel
-from sklearn.utils.validation import check_array
-from sklearn.utils.validation import check_is_fitted
 from sklearn.utils.validation import check_X_y
 
 
-class STLSQ(LinearModel, RegressorMixin):
+class BaseOptimizer(LinearModel, RegressorMixin):
     def __init__(
         self,
-        threshold=0.1,
         max_iter=100,
         normalize=False,
         fit_intercept=False,
-        threshold_intercept=False,
         copy_X=True
     ):
-        self.threshold = threshold
         self.max_iter = max_iter
         self.fit_intercept = fit_intercept
-        self.threshold_intercept = threshold_intercept
         self.normalize = normalize
         self.copy_X = copy_X
 
         self.history_ = []
+
+
+    # Force subclasses to implement this
+    @abc.abstractmethod
+    def _reduce(self):
+        '''Carry out the bulk of the work of the fit function'''
+        return
+
+
+    def fit(self, x_, y, sample_weight=None, **reduce_kws):
+        x_, y = check_X_y(x_, y, accept_sparse=[], y_numeric=True, multi_output=False)
+
+        x, y, X_offset, y_offset, X_scale = self._preprocess_data(
+            x_,
+            y,
+            fit_intercept=self.fit_intercept,
+            normalize=self.normalize,
+            copy=self.copy_X,
+            sample_weight=sample_weight,
+        )
+
+        if sample_weight is not None:
+            x, y = _rescale_data(x, y, sample_weight)
+
+        self.iters = 0
+        self.ind_ = np.ones(x.shape[1], dtype=bool)
+        self.coef_ = np.linalg.lstsq(x, y, rcond=None)[0]  # initial guess
+        self.history_.append(self.coef_)
+
+        self._reduce(x, y, **reduce_kws)
+
+        self._set_intercept(X_offset, y_offset, X_scale)
+        return self
+
+
+    @property
+    def complexity(self):
+        return np.count_nonzero(self.coef_) + np.count_nonzero([abs(self.intercept_) >= self.threshold])
+
+
+class STLSQ(BaseOptimizer):
+    def __init__(
+        self,
+        threshold=0.1,
+        alpha=0.0,
+        ridge_kw=None,
+        **kwargs
+    ):
+        super(STLSQ, self).__init__(**kwargs)
+        self.threshold = threshold
+        self.alpha = alpha
+        self.ridge_kw = ridge_kw
+
+        if threshold < 0:
+            raise ValueError('threshold must be nonnegative')
+        if alpha < 0:
+            raise ValueError('alpha must be nonnegative')
 
     def _sparse_coefficients(self, dim, ind, coef, threshold):
         c = np.zeros(dim)
@@ -42,7 +93,8 @@ class STLSQ(LinearModel, RegressorMixin):
         return c, big_ind
 
     def _regress(self, x, y):
-        coef = np.linalg.lstsq(x, y, rcond=None)[0]
+        kw = self.ridge_kw or {}
+        coef = ridge_regression(x, y, self.alpha, **kw)
         self.iters += 1
         return coef
 
@@ -87,68 +139,42 @@ class STLSQ(LinearModel, RegressorMixin):
         self.coef_ = coef
         self.ind_ = ind
 
-    def fit(self, x_, y, sample_weight=None):
-        x_, y = check_X_y(x_, y, accept_sparse=[], y_numeric=True, multi_output=False)
-
-        x, y, X_offset, y_offset, X_scale = self._preprocess_data(
-            x_,
-            y,
-            fit_intercept=self.fit_intercept,
-            normalize=self.normalize,
-            copy=self.copy_X,
-            sample_weight=sample_weight,
-        )
-
-        if sample_weight is not None:
-            x, y = _rescale_data(x, y, sample_weight)
-
-        self.iters = 0
-        self.ind_ = np.ones(x.shape[1], dtype=bool)  # initial guess
-        if self.threshold > 0:
-            self._reduce(x, y)
-        else:
-            self.coef_ = self._regress(x[:, self.ind_], y)
-
-        self._set_intercept(X_offset, y_offset, X_scale)
-        if self.threshold_intercept and abs(self.intercept_) < self.threshold:
-            self.intercept_ = 0
-        return self
-
-    @property
-    def complexity(self):
-        return np.count_nonzero(self.coef_) + np.count_nonzero([abs(self.intercept_) >= self.threshold])
+    # def _unbias(self, x, y):
+    #     if np.any(self.ind_):
+    #         coef = self._regress(x[:, self.ind_], y, 0)
+    #         self.coef_, self.ind_ = self._sparse_coefficients(x.shape[1], self.ind_, coef, self.threshold)
 
 
-class SR3(LinearModel, RegressorMixin):
+
+class SR3(BaseOptimizer):
     def __init__(
         self,
         threshold=0.1,
         nu=1.0,
-        max_iter=100,
-        normalize=False,
-        fit_intercept=False,
-        threshold_intercept=False,
-        copy_X=True
+        tol=1e-5,
+        **kwargs,
     ):
+        super(SR3, self).__init__(**kwargs)
         self.threshold = threshold
         self.nu = nu
-        self.max_iter = max_iter
-        self.fit_intercept = fit_intercept
-        self.threshold_intercept = threshold_intercept
-        self.normalize = normalize
-        self.copy_X = copy_X
+        self.tol = tol
 
-        self.history_ = []
+        if threshold < 0:
+            raise ValueError('threshold must be nonnegative')
+        if nu <= 0:
+            raise ValueError('nu must be positive')
+        if tol <= 0:
+            raise ValueError('tol must be positive')
 
-    def _update_unrelaxed_coef(self, x, y, coef_relaxed, nu):
-        A = np.dot(x.T, x) + np.eye(x.shape[1])/nu
-        b = np.dot(x.T, y) + coef_relaxed/nu
+    def _update_unrelaxed_coef(self, x, y, coef_relaxed):
+        A = np.dot(x.T, x) + np.eye(x.shape[1]) / self.nu
+        b = np.dot(x.T, y) + coef_relaxed / self.nu
         coef_unrelaxed = np.linalg.solve(A, b)
         self.iters += 1
         return coef_unrelaxed
 
-    def _update_relaxed_coef(self, coef_unrelaxed, threshold):
-        coef_relaxed = coef_unrelaxed*(np.abs(coef_unrelaxed) > threshold)
+    def _update_relaxed_coef(self, coef_unrelaxed):
+        coef_relaxed = coef_unrelaxed*(np.abs(coef_unrelaxed) > self.threshold)
         self.history_.append(coef_relaxed)
         return coef_relaxed
 
@@ -160,16 +186,16 @@ class SR3(LinearModel, RegressorMixin):
             last_coef = np.zeros_like(this_coef)
         return np.sum((this_coef - last_coef)**2)
 
-    def _reduce(self, x, y, tol):
+    def _reduce(self, x, y):
         """Iterates the thresholding. Assumes an initial guess is saved in self.coef_ and self.ind_"""
         coef_relaxed = self.coef_
         n_samples, n_features = x.shape
 
         for _ in range(self.iters, self.max_iter):
-            coef_unrelaxed = self._update_unrelaxed_coef(x, y, coef_relaxed, self.nu)
-            coef_relaxed = self._update_relaxed_coef(coef_unrelaxed, self.threshold)
+            coef_unrelaxed = self._update_unrelaxed_coef(x, y, coef_relaxed)
+            coef_relaxed = self._update_relaxed_coef(coef_unrelaxed)
 
-            if self._convergence_criterion() < tol:
+            if self._convergence_criterion() < self.tol:
                 # could not (further) select important features
                 break
         else:
@@ -185,172 +211,33 @@ class SR3(LinearModel, RegressorMixin):
         self.coef_ = coef_relaxed
         self.coef_unrelaxed_ = coef_unrelaxed
 
-    def fit(self, x_, y, sample_weight=None, convergence_tolerance=1e-5):
-        x_, y = check_X_y(x_, y, accept_sparse=[], y_numeric=True, multi_output=False)
-
-        x, y, X_offset, y_offset, X_scale = self._preprocess_data(
-            x_,
-            y,
-            fit_intercept=self.fit_intercept,
-            normalize=self.normalize,
-            copy=self.copy_X,
-            sample_weight=sample_weight,
-        )
-
-        if sample_weight is not None:
-            x, y = _rescale_data(x, y, sample_weight)
-
-        self.iters = 0
-        self.coef_ = np.linalg.lstsq(x, y, rcond=None)[0]  # initial guess
-        self.history_.append(self.coef_)
-        self._reduce(x, y, tol=convergence_tolerance)
-
-        self._set_intercept(X_offset, y_offset, X_scale)
-        if self.threshold_intercept and abs(self.intercept_) < self.threshold:
-            self.intercept_ = 0
-        return self
-
-    @property
-    def complexity(self):
-        return np.count_nonzero(self.coef_) + np.count_nonzero([abs(self.intercept_) >= self.threshold])
 
 
-
-class STRidge(LinearModel, RegressorMixin):
+class LASSO(BaseOptimizer):
     def __init__(
         self,
-        threshold=0.01,
-        alpha=0.1,
-        max_iter=100,
-        normalize=True,
-        fit_intercept=False,
-        threshold_intercept=False,
-        copy_X=True,
-        unbias=True,
-        ridge_kw=None,
+        alpha=1.0,
+        lasso_kw=None,
+        **kwargs
     ):
-        self.threshold = threshold
-        self.max_iter = max_iter
-        self.fit_intercept = fit_intercept
-        self.threshold_intercept = threshold_intercept
-        self.normalize = normalize
-        self.copy_X = copy_X
+        super(LASSO, self).__init__(**kwargs)
+        self.lasso_kw = lasso_kw
         self.alpha = alpha
-        self.unbias = unbias
-        self.ridge_kw = ridge_kw
 
-        self.history_ = []
+        if alpha < 0:
+            raise ValueError('alpha must be nonnegative')
 
-    def _sparse_coefficients(self, dim, ind, coef, threshold):
-        c = np.zeros(dim)
-        c[ind] = coef
-        big_ind = np.abs(c) >= threshold
-        c[~big_ind] = 0
-        self.history_.append(c)
-        return c, big_ind
-
-    def _regress(self, x, y, alpha):
-        kw = self.ridge_kw or {}
-        coef = ridge_regression(x, y, alpha, **kw)
-        self.iters += 1
-        return coef
-
-    def _no_change(self):
-        this_coef = self.history_[-1]
-        if len(self.history_) > 1:
-            last_coef = self.history_[-2]
-        else:
-            last_coef = np.zeros_like(this_coef)
-        return all(bool(i) == bool(j) for i, j in zip(this_coef, last_coef))
 
     def _reduce(self, x, y):
-        """Iterates the thresholding. Assumes an initial guess is saved in self.coef_ and self.ind_"""
-        ind = self.ind_
-        n_samples, n_features = x.shape
-        n_features_selected = sum(ind)
-
-        for _ in range(self.iters, self.max_iter):
-            if np.count_nonzero(ind) == 0:
-                warnings.warn(
-                    "Sparsity parameter is too big ({}) and eliminated all coeficients".format(self.threshold)
-                )
-                coef = np.zeros_like(ind, dtype=float)
-                break
-
-            coef = self._regress(x[:, ind], y, self.alpha)
-            coef, ind = self._sparse_coefficients(n_features, ind, coef, self.threshold)
-
-            if sum(ind) == n_features_selected or self._no_change():
-                # could not (further) select important features
-                break
-        else:
-            warnings.warn(
-                "STRidge._reduce did not converge after {} iterations.".format(self.max_iter),
-                ConvergenceWarning,
-            )
-            try:
-                coef
-            except NameError:
-                coef = self.coef_
-                warnings.warn("STRidge._reduce has no iterations left to determine coef", ConvergenceWarning)
-        self.coef_ = coef
-        self.ind_ = ind
-
-    def _unbias(self, x, y):
-        if np.any(self.ind_):
-            coef = self._regress(x[:, self.ind_], y, 0)
-            self.coef_, self.ind_ = self._sparse_coefficients(x.shape[1], self.ind_, coef, self.threshold)
-
-    def fit(self, x_, y, sample_weight=None):
-        x_, y = check_X_y(x_, y, accept_sparse=[], y_numeric=True, multi_output=False)
-
-        x, y, X_offset, y_offset, X_scale = self._preprocess_data(
-            x_,
-            y,
-            fit_intercept=self.fit_intercept,
-            normalize=self.normalize,
-            copy=self.copy_X,
-            sample_weight=sample_weight,
+        kw = self.lasso_kw or {}
+        lasso_model = Lasso(
+            alpha=self.alpha,
+            max_iter=self.max_iter,
+            fit_intercept=False,
+            **kw
         )
 
-        if sample_weight is not None:
-            x, y = _rescale_data(x, y, sample_weight)
+        lasso_model.fit(x, y)
 
-        self.iters = 0
-        self.ind_ = np.ones(x.shape[1], dtype=bool)  # initial guess
-        if self.threshold > 0:
-            self._reduce(x, y)
-        else:
-            self.coef_ = self._regress(x[:, self.ind_], y, self.alpha)
-
-        if self.unbias and self.alpha >= 0:
-            self._unbias(x, y)
-
-        self._set_intercept(X_offset, y_offset, X_scale)
-        if self.threshold_intercept and abs(self.intercept_) < self.threshold:
-            self.intercept_ = 0
-        return self
-
-    @property
-    def complexity(self):
-        return np.count_nonzero(self.coef_) + np.count_nonzero([abs(self.intercept_) >= self.threshold])
-
-
-def fit_with_noise(x, y, sigma, alpha, n, lmc=LinearRegression):
-    size = y.shape
-    n_samples, n_features = x.shape
-    beta_0 = np.mean(
-        [lmc().fit(x, y + np.random.normal(size=size, scale=sigma)).coef_ for _ in range(n)], axis=0
-    )
-    beta_init = lmc().fit(x, y).coef_
-
-    beta_sel = beta_init.copy()
-    mask = np.abs(beta_init) < alpha * np.abs(beta_0)
-    if np.all(mask):
-        raise FitFailedWarning("alpha too high: {}".format(alpha))
-    beta_sel[mask] = 0
-    model = lmc().fit(x[:, ~mask], y)
-    beta_sel[~mask] = model.coef_
-
-    return beta_sel, model.intercept_
-
+        self.coef = lasso_model.coef_
+        self.iters = lasso_model.n_iter_

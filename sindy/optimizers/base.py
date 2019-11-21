b28572f8e970417adbda97e0a1b604fd7815ba02
kpchamp <kpchamp@uw.edu>
2019-11-21 10:47:10 -0800

    Adding docstrings to BaseOptimizer

--- sindy/optimizers/base.py
+++ sindy/optimizers/base.py
@@ -12,6 +12,25 @@ from sklearn.linear_model.base import _rescale_data
 
 
 class BaseOptimizer(LinearModel, RegressorMixin):
+    """
+    Base class for SINDy optimizers. Subclasses must implement
+    a _reduce method for carrying out the bulk of the work of
+    fitting a model.
+
+    Parameters
+    ----------
+    fit_intercept : boolean, optional, default False
+        Whether to calculate the intercept for this model. If set to false, no
+        intercept will be used in calculations.
+
+    normalize : boolean, optional, default False
+        This parameter is ignored when fit_intercept is set to False. If True,
+        the regressors X will be normalized before regression by subtracting the
+        mean and dividing by the l2-norm.
+
+    copy_X : boolean, optional, default True
+        If True, X will be copied; else, it may be overwritten.
+    """
     def __init__(
         self,
         normalize=False,
@@ -35,6 +54,28 @@ class BaseOptimizer(LinearModel, RegressorMixin):
         raise NotImplementedError
 
     def fit(self, x_, y, sample_weight=None, **reduce_kws):
+        """
+        Fit the model.
+
+        Parameters
+        ----------
+        x_ : array-like, shape (n_samples, n_features)
+            Training data
+
+        y : array-like, shape (n_samples,) or (n_samples, n_targets)
+            Target values
+
+        sample_weight : float or numpy array of shape (n_samples,), optional
+            Individual weights for each sample
+
+        reduce_kws : dict
+            Optional keyword arguments to pass to the _reduce method
+            (implemented by subclasses)
+
+        Returns
+        -------
+        self : returns an instance of self
+        """
         x_, y = check_X_y(
             x_,
             y,

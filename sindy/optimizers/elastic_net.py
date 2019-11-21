2da48f3aec58a80eddef3ee4ebfe34e048cb9c6e
kpchamp <kpchamp@uw.edu>
2019-11-21 11:18:16 -0800

    adding docstrings for lasso and elastic net optimizations

--- sindy/optimizers/elastic_net.py
+++ sindy/optimizers/elastic_net.py
@@ -4,6 +4,40 @@ from sindy.optimizers import BaseOptimizer
 
 
 class ElasticNet(BaseOptimizer):
+    """
+    Linear regression with combined L1 and L2 regularization.
+
+    Minimizes the objective function
+    1 / (2 * n_samples) * ||y - Xw||^2_2
+    + alpha * l1_ratio * ||w||_1
+    + 0.5 * alpha * (1 - l1_ratio) * ||w||^2_2.
+
+    Parameters
+    ----------
+    alpha : float, optional, default 1
+        Strength of the L2 regularization
+
+    l1_ratio : float, optional, default 0.5
+        The ElasticNet mixing parameter, with 0 <= l1_ratio <= 1. For
+        l1_ratio = 0 the penalty is an L2 penalty. For l1_ratio = 1 it is
+        an L1 penalty. For 0 < l1_ratio < 1, the penalty is a combination
+        of L1 and L2.
+
+    max_iter : int, optional, default 1000
+        Maximum iterations of the optimization algorithm.
+
+    elastic_net_kw : dict, optional
+        Optional keyword arguments to pass to the ElasticNet regression
+        object.
+
+    Attributes
+    ----------
+    coef_ : array, shape (n_features,) or (n_targets, n_features)
+        Weight vector(s)
+
+    iters : int
+        Number of iterations performed in the optimization
+    """
     def __init__(
         self,
         alpha=1.0,
@@ -27,6 +61,8 @@ class ElasticNet(BaseOptimizer):
         self.elastic_net_kw = elastic_net_kw
 
     def _reduce(self, x, y):
+        """Performs the elastic net regression
+        """
         kw = self.elastic_net_kw or {}
         elastic_net_model = SKElasticNet(
             alpha=self.alpha,
--- sindy/optimizers/lasso.py
+++ sindy/optimizers/lasso.py
@@ -4,6 +4,32 @@ from sindy.optimizers import BaseOptimizer
 
 
 class LASSO(BaseOptimizer):
+    """
+    LASSO algorithm (linear regression with L1 regularization).
+
+    Attempts to minimize the objective function
+    (1 / (2 * n_samples)) * ||y - Xw||^2_2 + alpha * ||w||_1.
+
+    Parameters
+    ----------
+    alpha : float, optional, default 1
+        Strength of the L1 regularization
+
+    max_iter : int, optional, default 1000
+        Maximum iterations of the optimization algorithm.
+
+    lasso_kw : dict, optional
+        Optional keyword arguments to pass to the LASSO regression
+        object.
+
+    Attributes
+    ----------
+    coef_ : array, shape (n_features,) or (n_targets, n_features)
+        Weight vector(s)
+
+    iters : int
+        Number of iterations performed in the optimization
+    """
     def __init__(
         self,
         alpha=1.0,
@@ -23,6 +49,8 @@ class LASSO(BaseOptimizer):
         self.max_iter = max_iter
 
     def _reduce(self, x, y):
+        """Performs the LASSO regression
+        """
         kw = self.lasso_kw or {}
         lasso_model = Lasso(
             alpha=self.alpha,

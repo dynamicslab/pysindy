548782803939d86053fc6e2ecb1bb5a58a20c800
kpchamp <kpchamp@uw.edu>
2019-11-22 21:23:53 -0800

    add fit function to base feature library class (so subclasses are forced to implement it)

--- sindy/feature_library/feature_library.py
+++ sindy/feature_library/feature_library.py
@@ -11,8 +11,44 @@ class BaseFeatureLibrary():
         -addition (concatenate lists of features)
         -
     """
-    def __init__(self):
+    def __init__(self, **kwargs):
         pass
+
+    # Force subclasses to implement this
+    @abc.abstractmethod
+    def fit(self, X, y=None):
+        """
+        Compute number of output features.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The data.
+
+        Returns
+        -------
+        self : instance
+        """
+        raise NotImplementedError
+
+    # Force subclasses to implement this
+    @abc.abstractmethod
+    def transform(self, X):
+        """
+        Transform data to polynomial features
+
+        Parameters
+        ----------
+        X : array-like, shape [n_samples, n_features]
+            The data to transform, row by row.
+
+        Returns
+        -------
+        XP : np.ndarray, [n_samples, n_output_features]
+            The matrix of features, where n_output_features is the number
+            of features generated from the combination of inputs.
+        """
+        raise NotImplementedError
 
     # Some kind of function that applies the library
     def fit_transform(self, x):

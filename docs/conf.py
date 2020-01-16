9e905dff37e33e15f657f86b0bcba1a431beb075
Markus Quade <markus.quade@ambrosys.de>
2020-01-16 12:21:19 +0100

    add sphinx skeleton

---
+++ docs/conf.py
@@ -0,0 +1,60 @@
+import datetime
+import importlib
+import pathlib
+
+
+author = "dynamicslab"
+project = "pysindy"  # package name
+
+
+#### no need to edit below this line ##
+
+copyright = f"{datetime.datetime.now().year}, {author}"
+
+module = importlib.import_module(project)
+version = release = getattr(module, "__version__")
+
+master_doc = "index"
+
+extensions = [
+    "sphinxcontrib.apidoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+]
+
+apidoc_module_dir = f"../{project}"
+apidoc_excluded_paths = ["tests"]
+apidoc_toc_file = False
+
+autodoc_default_options = {"members": True}
+autodoc_member_order = "bysource"
+autoclass_content = "init"
+
+language = None
+
+here = pathlib.Path(__file__).parent
+
+if (here / "static/custom.css").exists():
+
+    html_static_path = ["static"]
+
+    def setup(app):
+        app.add_stylesheet("custom.css")
+
+
+exclude_patterns = ["build", "_build"]
+# pygments_style = "sphinx"
+
+add_module_names = True
+add_function_parentheses = False
+todo_include_todos = True
+
+html_theme = "sphinx_rtd_theme"
+html_show_sourcelink = False
+html_show_sphinx = False
+html_show_copyright = True
+
+default_role = "any"
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+Overview
+--------
+
+
+.. include:: ../README.rst
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: User Guide
+
+   API Documentation <api/pysindy>
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Useful links
+
+
+   PySINDy @ PyPI <https://pypi.org/>
+   Issue Tracker <https://github.com/dynamicslab/pysindy/issues>

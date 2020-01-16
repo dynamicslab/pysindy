ee839b72645da88c4c07174d8c38ee14af5fd923
Markus Quade <markus.quade@ambrosys.de>
2020-01-16 11:19:59 +0100

    Use setuptools_scm for tag based versioning

--- pysindy/__init__.py
+++ pysindy/__init__.py
@@ -1,3 +1,11 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass
+
+
 from pysindy.pysindy import SINDy
 from pysindy.differentiation import *
 from pysindy.optimizers import *

7649b1426a395feebd71fa29a22c9db64059f969
kpchamp <kpchamp@uw.edu>
2020-02-03 14:49:51 -0800

    Added an identity library

--- pysindy/feature_library/__init__.py
+++ pysindy/feature_library/__init__.py
@@ -1,3 +1,4 @@
+from .identity_library import IdentityLibrary
 from .custom_library import CustomLibrary
 from .fourier_library import FourierLibrary
 from .polynomial_library import PolynomialLibrary

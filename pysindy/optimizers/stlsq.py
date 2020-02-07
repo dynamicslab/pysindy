64ae5e2b683998f64abcabf857811401ebcce566
kpchamp <kpchamp@uw.edu>
2020-02-07 11:53:58 -0800

    updated STLSQ to work without MultiOutputRegressor

--- pysindy/optimizers/stlsq.py
+++ pysindy/optimizers/stlsq.py
@@ -84,7 +84,6 @@ class STLSQ(BaseOptimizer):
         c[ind] = coef
         big_ind = np.abs(c) >= threshold
         c[~big_ind] = 0
-        self.history_.append(c)
         return c, big_ind
 
     def _regress(self, x, y):
@@ -98,9 +97,9 @@ class STLSQ(BaseOptimizer):
     def _no_change(self):
         """Check if the coefficient mask has changed after thresholding
         """
-        this_coef = self.history_[-1]
+        this_coef = self.history_[-1].flatten()
         if len(self.history_) > 1:
-            last_coef = self.history_[-2]
+            last_coef = self.history_[-2].flatten()
         else:
             last_coef = np.zeros_like(this_coef)
         return all(bool(i) == bool(j) for i, j in zip(this_coef, last_coef))
@@ -112,7 +111,8 @@ class STLSQ(BaseOptimizer):
         """
         ind = self.ind_
         n_samples, n_features = x.shape
-        n_features_selected = sum(ind)
+        n_targets = y.shape[1]
+        n_features_selected = np.sum(ind)
 
         for _ in range(self.max_iter):
             if np.count_nonzero(ind) == 0:
@@ -125,10 +125,17 @@ class STLSQ(BaseOptimizer):
                 coef = np.zeros_like(ind, dtype=float)
                 break
 
-            coef = self._regress(x[:, ind], y)
-            coef, ind = self._sparse_coefficients(n_features, ind, coef, self.threshold)
-
-            if sum(ind) == n_features_selected or self._no_change():
+            coef = np.zeros((n_targets, n_features))
+            for i in range(n_targets):
+                coef_i = self._regress(x[:, ind[i]], y[:, i])
+                coef_i, ind_i = self._sparse_coefficients(
+                        n_features, ind[i], coef_i, self.threshold
+                    )
+                coef[i] = coef_i
+                ind[i] = ind_i
+            
+            self.history_.append(coef)
+            if np.sum(ind) == n_features_selected or self._no_change():
                 # could not (further) select important features
                 break
         else:

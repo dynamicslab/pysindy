1b2551ba223434a3fda51f31b4c40e4b1c5d0859
kpchamp <kpchamp@uw.edu>
2020-02-12 08:51:42 -0800

    reordering imports

--- pysindy/optimizers/__init__.py
+++ pysindy/optimizers/__init__.py
@@ -1,6 +1,6 @@
 from .base import BaseOptimizer
-from .sindy_optimizer import SINDyOptimizer
 from .elastic_net import ElasticNet
 from .lasso import LASSO
+from .sindy_optimizer import SINDyOptimizer
 from .sr3 import SR3
 from .stlsq import STLSQ

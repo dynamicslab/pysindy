8c6d0125b2405609c73a5f18ce2a5c7b7627d87b
kpchamp <kpchamp@uw.edu>
2020-02-12 13:15:41 -0800

    removing LASSO and ElasticNet from __init__.py file

--- pysindy/optimizers/__init__.py
+++ pysindy/optimizers/__init__.py
@@ -1,6 +1,4 @@
 from .base import BaseOptimizer
-from .elastic_net import ElasticNet
-from .lasso import LASSO
 from .sindy_optimizer import SINDyOptimizer
 from .sr3 import SR3
 from .stlsq import STLSQ

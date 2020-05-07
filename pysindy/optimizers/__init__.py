b963463584c5613c9217b5cf0075f72131952c2e
akaptano <akaptano@akap.hit>
2020-05-07 08:41:43 -0700

    Starting my own branch containing the implementation of the SR3 algorithm with linear equality constraints, as well as my additions to the SINDy algorithms using variable thresholding and initial guesses to the optimization problem.

--- pysindy/optimizers/__init__.py
+++ pysindy/optimizers/__init__.py
@@ -1,6 +1,7 @@
 from .base import BaseOptimizer
 from .sindy_optimizer import SINDyOptimizer
 from .sr3 import SR3
+from .sr3_enhanced import SR3Enhanced
 from .stlsq import STLSQ
 
 __all__ = ["BaseOptimizer", "SINDyOptimizer", "SR3", "STLSQ"]

7db4fd4c4e64a9b7e870891103027f9e5fbb525e
briandesilva <bdesilva@uw.edu>
2020-08-12 18:28:27 -0700

    Docstrings and minor updates

--- pysindy/scikit_time/scikit_time.py
+++ pysindy/scikit_time/scikit_time.py
@@ -9,7 +9,55 @@ from ..pysindy import SINDy
 
 class SINDyEstimator(SINDy):
     """
-    Implementation of the Scikit-time API for PySINDy.
+    Implementation of SINDy conforming to the API of a Scikit-time Estimator.
+
+    Parameters
+    ----------
+    optimizer : optimizer object, optional
+        Optimization method used to fit the SINDy model. This must be an object
+        extending the ``sindy.optimizers.BaseOptimizer`` class. Default is
+        sequentially thresholded least squares with a threshold of 0.1.
+
+    feature_library : feature library object, optional
+        Feature library object used to specify candidate right-hand side features.
+        This must be an object extending the
+        ``sindy.feature_library.BaseFeatureLibrary`` class.
+        Default is polynomial features of degree 2.
+
+    differentiation_method : differentiation object, optional
+        Method for differentiating the data. This must be an object extending
+        the ``sindy.differentiation_methods.BaseDifferentiation`` class.
+        Default is centered difference.
+
+    feature_names : list of string, length n_input_features, optional
+        Names for the input features (e.g. ``['x', 'y', 'z']``). If None, will use
+        ``['x0', 'x1', ...]``.
+
+    t_default : float, optional (default 1)
+        Default value for the time step.
+
+    discrete_time : boolean, optional (default False)
+        If True, dynamical system is treated as a map. Rather than predicting
+        derivatives, the right hand side functions step the system forward by
+        one time step. If False, dynamical system is assumed to be a flow
+        (right-hand side functions predict continuous time derivatives).
+
+    n_jobs : int, optional (default 1)
+        The number of parallel jobs to use when fitting, predicting with, and
+        scoring the model.
+
+    Attributes
+    ----------
+    model : sklearn.multioutput.MultiOutputRegressor object
+        The fitted SINDy model.
+
+    n_input_features_ : int
+        The total number of input features.
+
+    n_output_features_ : int
+        The total number of output features. This number is a function of
+        ``self.n_input_features`` and the feature library being used.
+
     """
 
     def __init__(
@@ -47,7 +95,7 @@ class SINDyEstimator(SINDy):
 
     def fetch_model(self):
         """
-        Yields the estimated model. Can be none if :meth:`fit` was note called.
+        Yields the estimated model. Can be none if :meth:`fit` was not called.
 
         Returns
         -------
@@ -57,17 +105,75 @@ class SINDyEstimator(SINDy):
         return self._model
 
     @property
-    def has_model(self) -> bool:
+    def has_model(self):
         """Property reporting whether this estimator contains an estimated
-        model. This assumes that the model is initialized with `None` otherwise.
+        model. This assumes that the model is initialized with ``None`` otherwise.
+
         :type: bool
         """
-        return self.model is not None
+        return self._model is not None
 
 
 class SINDyModel(SINDy):
     """
-    Implementation of the Scikit-time API for PySINDy.
+    Implementation of SINDy conforming to the API of a Scikit-time Model.
+
+    The model is represented as a Scikit-learn pipeline object with two steps:
+    1. Map the raw input data to nonlinear features according to the selected
+    ``feature_library``
+    2. Multiply the nonlinear features with a coefficient matrix encapuslated
+    in ``optimizer``.
+
+    This class expects the feature library and optimizer to already be fit
+    with a ``SINDyEstimator``. It is best to instantiate a ``SINDyModel``
+    object via the ``SINDyEstimator.fetch_model()`` rather than calling
+    the ``SINDyModel`` constructor directly.
+
+    Parameters
+    ----------
+    optimizer : optimizer object
+        Optimization method used to fit the SINDy model. This must be an
+        (already fit) object extending the ``pysindy.optimizers.BaseOptimizer``
+        class.
+
+    feature_library : feature library object
+        Feature library object used to specify candidate right-hand side features.
+        This must be an (already fit) object extending the
+        ``pysindy.feature_library.BaseFeatureLibrary`` class.
+
+    differentiation_method : differentiation object
+        Method for differentiating the data. This must be an object extending
+        the ``pysindy.differentiation_methods.BaseDifferentiation`` class.
+        Default is centered difference.
+
+    feature_names : list of string, length n_input_features, optional
+        Names for the input features (e.g. ``['x', 'y', 'z']``). If None, will use
+        ``['x0', 'x1', ...]``.
+
+    t_default : float, optional (default 1)
+        Default value for the time step.
+
+    discrete_time : boolean, optional (default False)
+        If True, dynamical system is treated as a map. Rather than predicting
+        derivatives, the right hand side functions step the system forward by
+        one time step. If False, dynamical system is assumed to be a flow
+        (right-hand side functions predict continuous time derivatives).
+
+    n_jobs : int, optional (default 1)
+        The number of parallel jobs to use when fitting, predicting with, and
+        scoring the model.
+
+    Attributes
+    ----------
+    model : sklearn.multioutput.MultiOutputRegressor object
+        The fitted SINDy model.
+
+    n_input_features_ : int
+        The total number of input features.
+
+    n_output_features_ : int
+        The total number of output features. This number is a function of
+        ``self.n_input_features`` and the feature library being used.
     """
 
     def __init__(
@@ -96,3 +202,14 @@ class SINDyModel(SINDy):
 
         self.n_input_features_ = self.model.steps[0][1].n_input_features_
         self.n_output_features_ = self.model.steps[0][1].n_output_features_
+
+    def copy(self):
+        """ Makes a deep copy of this model.
+        Returns
+        -------
+        copy: SINDyModel
+            A new copy of this model.
+        """
+        import copy
+
+        return copy.deepcopy(self)

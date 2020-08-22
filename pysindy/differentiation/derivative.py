a2a530275fd9a4728c3ff6022fdf2f606de63ad0
briandesilva <bdesilva@uw.edu>
2020-08-22 14:52:09 -0700

    Refactor derivative wrapper based on xgboost implementation

--- pysindy/differentiation/derivative.py
+++ pysindy/differentiation/derivative.py
@@ -9,6 +9,7 @@ from derivative.dglobal import TrendFiltered
 from derivative.differentiation import Derivative
 from derivative.dlocal import FiniteDifference
 from derivative.dlocal import SavitzkyGolay
+from derivative import dxdt
 from numpy import arange
 from numpy import vectorize
 from sklearn.base import BaseEstimator
@@ -43,21 +44,53 @@ class SINDyDerivative(BaseEstimator):
 
     Parameters
     ----------
-    derivative_class: subclass of ``derivative.Derivative``
-        The derivative class to be wrapped.
-        Must be inherit from the ``Derivative`` class of the ``derivative`` package.
+    derivative_kws: dictionary, optional
+        Keyword arguments to be passed to the ``derivative.dxdt`` method.
+
+    Notes
+    -----
+    See the `derivative documentation <https://derivative.readthedocs.io/en/latest/>`_
+    for acceptable keywords.
     """
 
-    def __init__(self, derivative_class):
-        if isinstance(derivative_class, Derivative):
-            self.derivative_class = derivative_class
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def set_params(self, **params):
+        """
+        Set the parameters of this estimator.
+        Modification of the pysindy method to allow unknown kwargs. This allows using
+        the full range of derivative parameters that are not defined as member variables
+        in sklearn grid search.
+
+        Returns
+        -------
+        self
+        """
+        if not params:
+            # Simple optimization to gain speed (inspect is slow)
+            return self
+        else:
+            self.kwargs.update(params)
+
+        return self
+
+    def get_params(self, deep=True):
+        """Get parameters."""
+        params = super().get_params(deep)
+
+        if isinstance(self.kwargs, dict):
+            params.update(self.kwargs)
+
+        return params
 
     def _differentiate(self, x, t=1):
         if isinstance(t, (int, float)):
             if t < 0:
                 raise ValueError("t must be a positive constant or an array")
             t = arange(x.shape[0]) * t
-        return self.derivative_class.d(x, t, axis=0)
+
+        return dxdt(x, t, axis=0, **self.kwargs)
 
     def __call__(self, x, t=1):
         x = validate_input(x, t=t)

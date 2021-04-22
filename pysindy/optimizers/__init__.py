d8819b4d12d44daac7c882d6aa30b5edc718eb97
akaptano <akaptano@akap.hit>
2021-04-21 17:02:39 -0700

    Edited the __init__ file that was causing the linting error, hoping this is close to fixing any linting problems.

--- pysindy/optimizers/__init__.py
+++ pysindy/optimizers/__init__.py
@@ -1,8 +1,15 @@
 from .base import BaseOptimizer
 from .constrained_sr3 import ConstrainedSR3
-from .trapping_sr3 import trappingSR3
 from .sindy_optimizer import SINDyOptimizer
 from .sr3 import SR3
 from .stlsq import STLSQ
+from .trapping_sr3 import trappingSR3
 
-__all__ = ["BaseOptimizer", "trappingSR3", "ConstrainedSR3", "SINDyOptimizer", "SR3", "STLSQ"]
+__all__ = [
+    "BaseOptimizer",
+    "SINDyOptimizer",
+    "SR3",
+    "STLSQ",
+    "ConstrainedSR3",
+    "trappingSR3",
+]

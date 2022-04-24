45435b8fa560037e33239920075139afb5b7524a
Alan Kaptanoglu <alankaptanoglu@Alans-MacBook-Air.local>
2022-04-24 11:21:46 -0700

    Fixed the simple linting errors.

--- pysindy/feature_library/weak_pde_library.py
+++ pysindy/feature_library/weak_pde_library.py
@@ -185,7 +185,7 @@ class WeakPDELibrary(BaseFeatureLibrary):
         if num_pts_per_domain is not None:
             raise DeprecationWarning(
                 "The parameter num_pts_per_domain is now deprecated. This "
-                "value will be ignored in favor of the newer code."
+                "value will be ignored."
             )
 
         # list of integrals
@@ -265,7 +265,7 @@ class WeakPDELibrary(BaseFeatureLibrary):
 
     def _set_up_grids(self):
         """
-            Sets up grids needed for the weak library.
+        Sets up grids needed for the weak library.
         """
         dims = self.spatiotemporal_grid.shape[:-1]
         self.grid_dims = dims
@@ -300,9 +300,7 @@ class WeakPDELibrary(BaseFeatureLibrary):
                 if len(newinds) < 2:
                     for i in range(self.grid_ndim):
                         self.domain_centers[k, i] = np.random.uniform(
-                            xt1[i] + self.H_xt[i],
-                            xt2[i] - self.H_xt[i],
-                            size=1
+                            xt1[i] + self.H_xt[i], xt2[i] - self.H_xt[i], size=1
                         )
                     include = False
                     break
@@ -432,7 +430,7 @@ class WeakPDELibrary(BaseFeatureLibrary):
                 dims = np.ones(self.grid_ndim, dtype=int)
                 dims[i] = self.shapes_k[k][i]
                 ret = ret * np.reshape(
-                    tweights[i][self.lefts[i][k]:self.rights[i][k] + 1], dims
+                    tweights[i][self.lefts[i][k] : self.rights[i][k] + 1], dims
                 )
 
             self.fulltweights = self.fulltweights + [
@@ -450,7 +448,7 @@ class WeakPDELibrary(BaseFeatureLibrary):
                 dims = np.ones(self.grid_ndim, dtype=int)
                 dims[i] = self.shapes_k[k][i]
                 ret = ret * np.reshape(
-                    weights0[i][self.lefts[i][k]:self.rights[i][k] + 1], dims
+                    weights0[i][self.lefts[i][k] : self.rights[i][k] + 1], dims
                 )
 
             self.fullweights0 = self.fullweights0 + [ret * np.product(self.H_xt_k[k])]
@@ -470,7 +468,7 @@ class WeakPDELibrary(BaseFeatureLibrary):
                     dims = np.ones(self.grid_ndim, dtype=int)
                     dims[i] = self.shapes_k[k][i]
                     ret = ret * np.reshape(
-                        weights1[j][i][self.lefts[i][k]:self.rights[i][k] + 1],
+                        weights1[j][i][self.lefts[i][k] : self.rights[i][k] + 1],
                         dims,
                     )
 
@@ -482,16 +480,16 @@ class WeakPDELibrary(BaseFeatureLibrary):
     @staticmethod
     def _combinations(n_features, n_args, interaction_only):
         """
-            Get the combinations of features to be passed to a library function.
+        Get the combinations of features to be passed to a library function.
         """
         comb = combinations if interaction_only else combinations_w_r
         return comb(range(n_features), n_args)
 
     def _phi(self, x, d, p):
         """
-            One-dimensional polynomial test function (1-x**2)**p,
-            differentiated d times, calculated term-wise in the binomial
-            expansion.
+        One-dimensional polynomial test function (1-x**2)**p,
+        differentiated d times, calculated term-wise in the binomial
+        expansion.
         """
         ks = np.arange(self.p + 1)
         ks = ks[np.where(2 * (self.p - ks) - d >= 0)][:, np.newaxis]
@@ -505,9 +503,9 @@ class WeakPDELibrary(BaseFeatureLibrary):
 
     def _phi_int(self, x, d, p):
         """
-            Indefinite integral of one-dimensional polynomial test
-            function (1-x**2)**p, differentiated d times, calculated
-            term-wise in the binomial expansion.
+        Indefinite integral of one-dimensional polynomial test
+        function (1-x**2)**p, differentiated d times, calculated
+        term-wise in the binomial expansion.
         """
         ks = np.arange(self.p + 1)
         ks = ks[np.where(2 * (self.p - ks) - d >= 0)][:, np.newaxis]
@@ -522,9 +520,9 @@ class WeakPDELibrary(BaseFeatureLibrary):
 
     def _xphi_int(self, x, d, p):
         """
-            Indefinite integral of one-dimensional polynomial test function
-            x*(1-x**2)**p, differentiated d times, calculated term-wise in the
-            binomial expansion.
+        Indefinite integral of one-dimensional polynomial test function
+        x*(1-x**2)**p, differentiated d times, calculated term-wise in the
+        binomial expansion.
         """
         ks = np.arange(self.p + 1)
         ks = ks[np.where(2 * (self.p - ks) - d >= 0)][:, np.newaxis]
@@ -539,11 +537,11 @@ class WeakPDELibrary(BaseFeatureLibrary):
 
     def _linear_weights(self, x, d, p):
         """
-            One dimensioal weights for integration against the dth derivative
-            of the polynomial test function (1-x**2)**p. This is derived
-            assuming the function to integrate is linear between grid points:
-            f(x)=f_i+(x-x_i)/(x_{i+1}-x_i)*(f_{i+1}-f_i)
-            so that f(x)*dphi(x) is a piecewise polynomial.
+        One dimensioal weights for integration against the dth derivative
+        of the polynomial test function (1-x**2)**p. This is derived
+        assuming the function to integrate is linear between grid points:
+        f(x)=f_i+(x-x_i)/(x_{i+1}-x_i)*(f_{i+1}-f_i)
+        so that f(x)*dphi(x) is a piecewise polynomial.
         """
         ws = self._phi_int(x, d, p)
         zs = self._xphi_int(x, d, p)
@@ -765,15 +763,10 @@ class WeakPDELibrary(BaseFeatureLibrary):
             n_samples_full = self.K * self.num_trajectories
 
         xp_full = np.empty(
-            (self.num_trajectories, n_samples, self.n_output_features_),
-            dtype=x.dtype
+            (self.num_trajectories, n_samples, self.n_output_features_), dtype=x.dtype
         )
         x_full = np.reshape(
-            x,
-            np.concatenate(
-                [[self.num_trajectories],
-                 self.grid_dims, [n_features]]
-                )
+            x, np.concatenate([[self.num_trajectories], self.grid_dims, [n_features]])
         )
 
         # Loop over each trajectory
@@ -994,7 +987,7 @@ class WeakPDELibrary(BaseFeatureLibrary):
                 if self.include_interaction:
                     xp[
                         :,
-                        library_idx:library_idx
+                        library_idx : library_idx
                         + n_library_terms * self.num_derivatives * n_features,
                     ] = library_mixed_integrals
                     library_idx += n_library_terms * self.num_derivatives * n_features

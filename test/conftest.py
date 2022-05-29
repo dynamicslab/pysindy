TST: Fix derivative tests expecting old implicit shape conversion.
Also, make SINDyDerivative a subclass of BaseDifferentiation so that it can
import __call__ rather than redefine it verbatim.  We expect SINDyDerivative to
expose the same interface as BaseDifferentiation with some additional logic, so
subclass is an appropriate relationship.

Also:
    clean up of a test that used old return_arrays = True arg in
        _process_multiple_trajectories()

test/conftest.py
@@ -208,7 +208,7 @@ def data_2d_resolved_pde():
 @pytest.fixture
 def data_derivative_1d():
     x = 2 * np.linspace(1, 100, 100)
-    x_dot = 2 * np.ones(100).reshape(-1, 1)
+    x_dot = 2 * np.ones(100)
     return x, x_dot
 
 
@@ -216,7 +216,7 @@ def data_derivative_1d():
 def data_derivative_quasiperiodic_1d():
     t = np.arange(1000) * 2 * np.pi / 1000
     x = 2 * np.sin(t)
-    x_dot = 2 * np.cos(t).reshape(-1, 1)
+    x_dot = 2 * np.cos(t)
     return t, x, x_dot
 
 

test/differentiation/test_differentiation_methods.py
@@ -252,7 +252,7 @@ def test_wrapper_equivalence_with_dxdt(data, derivative_kws):
 
     if np.ndim(x) == 1:
         np.testing.assert_allclose(
-            dxdt(x.reshape(-1, 1), t, axis=0, **derivative_kws),
+            dxdt(x, t, axis=0, **derivative_kws),
             SINDyDerivative(**derivative_kws)(x, t),
         )
     else:
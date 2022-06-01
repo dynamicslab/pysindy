BUG: zero-order derivatives in PDELibrary

pysindy/feature_library/pde_library.py
@@ -158,7 +158,9 @@ class PDELibrary(PDEShapedInputsMixin, BaseFeatureLibrary):
         indices = len(spatial_grid.ax_spatial) * (range(derivative_order + 1),)
 
         multiindices = [
-            ind for ind in iproduct(*indices) if np.sum(ind) <= derivative_order
+            ind
+            for ind in iproduct(*indices)
+            if (np.sum(ind) <= derivative_order and np.sum(ind) > 0)
         ]
         multiindices = np.array(multiindices)
         num_derivatives = len(multiindices)

pysindy/feature_library/weak_pde_library.py
@@ -228,7 +228,9 @@ class WeakPDELibrary(PDEShapedInputsMixin, BaseFeatureLibrary):
 
         # Calculate indices of coordinates involved in mixed partial derivatives
         multiindices = [
-            ind for ind in iproduct(*indices) if np.sum(ind) <= derivative_order
+            ind
+            for ind in iproduct(*indices)
+            if np.sum(ind) <= derivative_order and np.sum(ind) > 0
         ]
         multiindices = np.array(multiindices)
         num_derivatives = len(multiindices)
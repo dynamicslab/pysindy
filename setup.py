2ab4301c4b32c18bc5146a3b416bf70f7dc4ebcf
Jake Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2023-04-07 01:39:46 -0700

    DOC: Add trove classifiers for newer pythons

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ CLASSIFIERS = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Science/Research",
     "License :: OSI Approved :: MIT License",

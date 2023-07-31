fe3e4adec8faf13abee6460823b0125904d3baa3
Jake Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2023-07-31 10:43:07 -0700

    TST: Add test coverage for validate_control_variables
    Also:
        annotate and add docstring to validate_control_variables
        add AxesArray syntax

--- test/utils/test_utils.py
+++ test/utils/test_utils.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pytest
 
+from pysindy.utils import AxesArray
 from pysindy.utils import reorder_constraints
+from pysindy.utils import validate_control_variables
 
 
 def test_reorder_constraints_1D():
@@ -26,3 +29,17 @@ def test_reorder_constraints_2D():
     np.testing.assert_array_equal(
         reorder_constraints(row_order, n_feats, output_order="target"), target_order
     )
+
+
+def test_validate_controls():
+    with pytest.raises(ValueError):
+        validate_control_variables(1, [])
+    with pytest.raises(ValueError):
+        validate_control_variables([], 1)
+    with pytest.raises(ValueError):
+        validate_control_variables([], [1])
+    arr = AxesArray(np.ones(4).reshape((2, 2)), axes={"ax_time": 0, "ax_coord": 1})
+    with pytest.raises(ValueError):
+        validate_control_variables([arr], [arr[:1]])
+    u_mod = validate_control_variables([arr], [arr], trim_last_point=True)
+    assert u_mod[0].n_time == 1

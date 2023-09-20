1a688f5802b97e788eea2f1b42e99059b5925974
sarinstein-yan <98195226+sarinstein-yan@users.noreply.github.com>
2023-09-20 15:04:36 +0800

    Bugfix - axes.py

--- pysindy/utils/axes.py
+++ pysindy/utils/axes.py
@@ -22,7 +22,7 @@ class AxesArray(np.lib.mixins.NDArrayOperatorsMixin, np.ndarray):
         AxesWarning if axes does not match shape of input_array
     """
 
-    def __new__(cls, input_array, axes):
+    def __new__(cls, input_array, axes=None):
         obj = np.asarray(input_array).view(cls)
         defaults = {
             "ax_time": None,

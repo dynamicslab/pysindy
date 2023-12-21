5b60afebf70187d7fda8fe6292271deeacbdfd62
Mikkel Lykkegaard <mikkel@digilab.co.uk>
2023-12-21 10:43:38 +0000

    Vectorized sampling of SINDy coefficients in numpyro for optimization

--- pysindy/optimizers/sbr.py
+++ pysindy/optimizers/sbr.py
@@ -40,18 +40,8 @@ class SBR(BaseOptimizer):
         # set up a sparse regression and sample.
         self.mcmc = self._run_mcmc(x, y, **self.mcmc_kwargs)
 
-        # get the variable names and the mean values from the samples.
-        beta_names = [
-            varname
-            for varname in self.mcmc.get_samples().keys()
-            if varname.startswith("beta")
-        ]
-        beta = np.array(
-            [self.mcmc.get_samples()[varname].mean().item() for varname in beta_names]
-        ).reshape(y.shape[1], -1)
-
         # set the mean values as the coefficients.
-        self.coef_ = beta
+        self.coef_ = np.array(self.mcmc.get_samples()["beta"].mean(axis=0))
 
     def _numpyro_model(self, x, y):
         # get the dimensionality of the problem.
@@ -64,13 +54,7 @@ class SBR(BaseOptimizer):
         )
 
         # sample the parameters compute the predicted outputs.
-        beta = []
-        for i in range(n_targets):
-            beta_i = []
-            for j in range(n_features):
-                beta_i.append(sample_reg_horseshoe(i, j, tau, c_sq))
-            beta.append(beta_i)
-        beta = jnp.array(beta)
+        beta = sample_reg_horseshoe(tau, c_sq, (n_targets, n_features))
         mu = jnp.dot(x, beta.T)
 
         # compute the likelihood.
@@ -106,19 +90,17 @@ def sample_reg_horseshoe_hyper(tau0=0.1, nu=4, s=2):
     return tau, c_sq
 
 
-def sample_reg_horseshoe(i, j, tau, c_sq):
+def sample_reg_horseshoe(tau, c_sq, shape):
     """
     For details on this prior, please refer to:
     Hirsh, S. M., Barajas-Solano, D. A., & Kutz, J. N. (2021).
     parsifying Priors for Bayesian Uncertainty Quantification in
     Model Discovery (arXiv:2107.02107). arXiv. http://arxiv.org/abs/2107.02107
     """
-    lambda_i_j = numpyro.sample("lambda_{}_{}".format(i, j), HalfCauchy(1.0))
-    lambda_i_j_squiggle = (
-        jnp.sqrt(c_sq) * lambda_i_j / jnp.sqrt(c_sq + tau**2 * lambda_i_j**2)
+    lamb = numpyro.sample("lambda", HalfCauchy(1.0), sample_shape=shape)
+    lamb_squiggle = jnp.sqrt(c_sq) * lamb / jnp.sqrt(c_sq + tau**2 * lamb**2)
+    beta = numpyro.sample(
+        "beta",
+        Normal(jnp.zeros_like(lamb_squiggle), jnp.sqrt(lamb_squiggle**2 * tau**2)),
     )
-    beta_i_j = numpyro.sample(
-        "beta_{}_{}".format(i, j),
-        Normal(0.0, jnp.sqrt(lambda_i_j_squiggle**2 * tau**2)),
-    )
-    return beta_i_j
+    return beta

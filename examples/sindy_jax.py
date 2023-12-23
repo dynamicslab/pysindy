65712f604bb13f0f1ae8dc0c381f38f0a8b87527
charlie <charlie.chen@nyu.edu>
2023-12-23 11:59:46 -0500

    Add predict function

--- examples/sindy_jax.py
+++ examples/sindy_jax.py
@@ -40,11 +40,35 @@ def lorenz_system(X, t, theta):
     dz_dt = X[0] * X[1] - theta[2] * X[2]
     return jnp.array([dx_dt, dy_dt, dz_dt])
 
+def predict(system, data, theta, t):
+    x0 = data[0, :, :]
+    n_tsteps = data.shape[0]
+    t_pred = t[:n_tsteps]
 
-def cost_function(system, data, theta, ntime, t, l1param=None):
+    # Solve system ODE with parameters theta, initial condition x0, and time t_pred
+    # TODO: this should be more general, we should not be able to know 
+    # the structure of the system.
+    x = odeint(system, x0, t_pred, theta)
+    return x
+
+
+
+def cost_function(predicted, data, theta, l1param=None):
+    error = (predicted - data)**2
+    error = jnp.average(error, axis=0, weights=jax.nn.relu(1-(0.05*jnp.arange(error.shape[0]))))
+    loss = jnp.average(error, axis=1).sum()
+    # from IPython import embed
+    # embed() or exit(0)
+    # loss = jnp.mean((predicted-data)**2, axis=[0, 2]).sum()
+    if l1param is not None:
+        loss += l1param * jnp.sum(jnp.abs(theta))
+    return loss
+
+
+def old_cost_function(system, data, theta, t, l1param=None):
     # Integrate the system forward in time and evaluate loss
     n_tsteps = data.shape[0]
-    n_examples = ntime
+    n_examples = data.shape[2]
     t_pred = t[:n_tsteps]
     x0 = data[0, :, :]
     print("data shape = ", data.shape)
@@ -60,22 +84,20 @@ def cost_function(system, data, theta, ntime, t, l1param=None):
 
     return loss
 
+# - What is the role of t?
+# x_shape (N, # feature)
 
 x0 = jnp.array([1.0, 1.0, 1.0])
-for ntime in [1000, 10000]:
+for ntime in [5000]:
     t = jnp.linspace(0, 20, ntime)
     sol = odeint(lorenz_system, x0, t, (10, 28, 8 / 3))
-    sys_cost_function = lambda H, coefs: cost_function(
-        lorenz_system, H, coefs, l1param=0.0, ntime=ntime, t=t
-    )
 
     for n_tstep in [2, 10, 100]:
         print("ntime, n_tstep = ", ntime, n_tstep)
         H = multi_hankel_matrix2(sol, n_tstep)
-        cf_jit = jax.jit(sys_cost_function)
 
         # optimization parameters
-        epochs = 4000
+        epochs = 1000
         learning_rate = 0.2
 
         # Define the optimizer
@@ -90,10 +112,20 @@ for ntime in [1000, 10000]:
         # Define the loss function
         # compute_loss = lambda params, data: cost_function(lorenz_system, data, params['theta'], l1param=0.01)
 
+        # Define predict function
+        sys_predict = lambda params, data: predict(lorenz_system, data, params['theta'], t=t)
         # Define the loss function
-        compute_loss = lambda params, data: cf_jit(data, params["theta"])
-        cl_jit = jax.jit(compute_loss)
-        grads_jit = jax.jit(jax.grad(cl_jit))
+        compute_loss = lambda params, data: cost_function(
+            sys_predict(params, data),
+            data,
+            params['theta'],
+            l1param=0.0
+        )
+
+        # jit compilation
+        jit_predict = jax.jit(sys_predict)        
+        jit_loss = jax.jit(compute_loss)
+        jit_grad_loss = jax.jit(jax.grad(compute_loss))
 
         train_ratio = 0.1
         nsamp = H.shape[2]
@@ -109,17 +141,29 @@ for ntime in [1000, 10000]:
             idxs = random.sample(range(nsamp), batch)
             Hsub = H[:, :, idxs]
 
+            # # Get predictions
+            # if i > epochs-10: 
+            #     preds = jit_predict(params, Hsub)
+            #     error = ((preds - Hsub)[1]**2).mean(axis=0)
+            #     plt.scatter(idxs, error)
+            #     plt.show()
+            # if i == epochs-2:
+            #     exit(0)
+
             # Update the parameters
-            grads = grads_jit(params, Hsub)
+            grads = jit_grad_loss(params, Hsub)
             updates, opt_state = optimizer.update(grads, opt_state)
             params = optax.apply_updates(params, updates)
 
             if i % print_iter == 0:
-                print("Epoch: ", i)
-                print("params: ", params["theta"])
-                loss[i // print_iter] = cl_jit(params, Hsub)
-                print("Loss :", loss[i // print_iter])
-                print("-------")
+                # print("Epoch: ", i)
+                # print("params: ", params["theta"])
+                loss[i // print_iter] = jit_loss(params, Hsub)
+                # print("Loss :", loss[i // print_iter])
+                # print("-------")
+
+        metric = ((params['theta'] - jnp.array([10, 28, 8 / 3]))**2).sum()
+        print("Metric:", metric)
 
         t2 = time.time()
         print("Final params: ", params["theta"])
@@ -138,12 +182,12 @@ for ntime in [1000, 10000]:
         plt.figure(3)
         plt.semilogy(
             t2 - t1,
-            loss[-1],
+            metric,
             "o",
             label="ntime = " + str(ntime) + ", n_tstep = " + str(n_tstep),
         )
         plt.grid(True)
         plt.xlabel("t (s)")
-        plt.ylabel("objective")
+        plt.ylabel("metric")
         plt.legend()
 plt.show()

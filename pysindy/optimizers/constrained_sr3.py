3436aedd89f51228a9fe139c8895f6e31463ce4f
Jake Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2024-05-29 17:50:18 -0700

    Backport changes to BaseOptimizer, SR3, and ConstrainedSR3

--- pysindy/optimizers/constrained_sr3.py
+++ pysindy/optimizers/constrained_sr3.py
@@ -300,8 +300,6 @@ class ConstrainedSR3(SR3):
                     <= self.constraint_rhs[: self.constraint_separation_index]
                 )
             prob = cp.Problem(cp.Minimize(cost), constraints)
-        else:
-            cp.Problem(cp.Minimize(cost))
 
         prob_clone = deepcopy(prob)
         # default solver is SCS/OSQP here but switches to ECOS for L2

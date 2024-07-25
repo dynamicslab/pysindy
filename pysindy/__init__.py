dd4e1edc7ef0094afcf19c4f35c6cd13eb4fa341
himkwtn <him10424@gmail.com>
2024-07-25 11:39:45 -0700

    CLN: fix linting

--- pysindy/__init__.py
+++ pysindy/__init__.py
@@ -1,4 +1,5 @@
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version
 
 try:
     __version__ = version(__name__)

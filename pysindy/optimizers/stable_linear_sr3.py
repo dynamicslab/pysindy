73e103c229134ab61d8620cb7d860e6506de8509
himkwtn <him10424@gmail.com>
2024-08-13 14:42:12 -0700

    CLN(sr3): remove duplicated code in child class

--- pysindy/optimizers/stable_linear_sr3.py
+++ pysindy/optimizers/stable_linear_sr3.py
@@ -320,39 +320,6 @@ class StableLinearSR3(ConstrainedSR3):
             A_temp[r:, :r] = A_old[r:, :r]
             return A_temp.T
 
-    def _objective(
-        self, x, y, q, coef_negative_definite, coef_sparse, trimming_array=None
-    ):
-        """Objective function"""
-        if q != 0:
-            print_ind = q % (self.max_iter // 10.0)
-        else:
-            print_ind = q
-        R2 = (y - np.dot(x, coef_negative_definite)) ** 2
-        D2 = (coef_negative_definite - coef_sparse) ** 2
-        if self.use_trimming:
-            assert trimming_array is not None
-            R2 *= trimming_array.reshape(x.shape[0], 1)
-
-        regularization = self.reg(
-            coef_negative_definite,
-            (self.threshold**2 if self.thresholds is None else self.thresholds**2)
-            / self.nu,
-        )
-        if print_ind == 0 and self.verbose:
-            row = [
-                q,
-                np.sum(R2),
-                np.sum(D2) / self.nu,
-                regularization,
-                np.sum(R2) + np.sum(D2) + regularization,
-            ]
-            print(
-                "{0:10d} ... {1:10.4e} ... {2:10.4e} ... {3:10.4e}"
-                " ... {4:10.4e}".format(*row)
-            )
-        return 0.5 * np.sum(R2) + 0.5 * regularization + 0.5 * np.sum(D2) / self.nu
-
     def _reduce(self, x, y):
         """
         Perform at most ``self.max_iter`` iterations of the SR3 algorithm

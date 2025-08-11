7676586b826997534271713f96d81654521ffcb0
Yash Bhangale <yash6599@uw.edu>
2025-08-11 14:31:29 -0700

    CLN: lint code

--- test/test_sindyc.py
+++ test/test_sindyc.py
@@ -239,7 +239,7 @@ def test_score_multiple_trajectories(data_multiple_trajectories):
     model = SINDy()
     model.fit(x, u=u, t=t)
     check_is_fitted(model)
-    
+
     s = model.score(x, u=u, t=t)
     assert s <= 1
 
